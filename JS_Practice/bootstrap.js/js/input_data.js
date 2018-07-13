b9e116796a6586d2f67e7c8a6a34eaac1429d611
ravi504 <ravinder.rsr@gmail.com>
2018-07-13 17:56:41 +0530

    Add javascript.info

--- JS_Practice/bootstrap.js/js/input_data.js
+++ JS_Practice/bootstrap.js/js/input_data.js
@@ -2,17 +2,24 @@
 	
 	function check_valid(){
 		var forms = document.getElementsByClassName('need-validation');
-		
+		var user = document.getElementById('validation1');
+		var pass = document.getElementById('validation2');
+		var user_detil = [];
 		var validation = Array.prototype.filter.call(forms, function(form){
 			form.addEventListener('submit', function(event){
 				if(form.checkValidity() === false){
 					event.preventDefault();
 					event.stopPropagation();
+				}else{
+					user_detil.push("user:" + user.value + "pass:" + pass.value );
+					alert(user_detil[0]);
 				}
 				form.classList.add('was-validated');
 			},false);
 		});
 	}
-	
+			
 	window.addEventListener('load', check_valid, false);
+	
+	
 })();
